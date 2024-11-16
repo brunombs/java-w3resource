4139cd0a8615828aec63627e50d45e271ed0804a
brunombs <brunozs@msn.com>
2024-11-16 18:43:03 -0300

    improving exercise

--- 05 - Object Oriented Programming/04 - Java Inheritance/06 - AnimalRunMove/MainAnimalRunMove.java
+++ 05 - Object Oriented Programming/04 - Java Inheritance/06 - AnimalRunMove/MainAnimalRunMove.java
@@ -1,3 +1,5 @@
+// Write a Java program to create a class called Animal with a method named move(). Create a subclass called Cheetah that overrides the move() method to run.
+
 public class MainAnimalRunMove {
     public static void main(String[] args) {
         Cheetah cheetah = new Cheetah();
