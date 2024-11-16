05b4fa5de2bd102638848f74eed8ebd551983767
brunombs <brunozs@msn.com>
2024-11-15 23:40:00 -0300

    improving exercise

--- 05 - Object Oriented Programming/04 - Java Inheritance/05 - BankAccount/BankAccountv2.java
+++ 05 - Object Oriented Programming/04 - Java Inheritance/05 - BankAccount/BankAccountv2.java
@@ -1,7 +1,26 @@
 public class BankAccountv2 {
-    void deposit(){
+    private String accountNumber;
+    private double balance;
+
+    public BankAccountv2(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
     }
 
-    void withdraw(){
+    void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    void withdraw(double amount) {
+        if(balance >= amount) {
+            this.balance -= amount;
+        }
+        else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
     }
 }
--- 05 - Object Oriented Programming/04 - Java Inheritance/05 - BankAccount/SavingsAccount.java
+++ 05 - Object Oriented Programming/04 - Java Inheritance/05 - BankAccount/SavingsAccount.java
@@ -1,2 +1,15 @@
-public class SavingsAccount {
+public class SavingsAccount extends BankAccountv2{
+    public SavingsAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+
+    @Override
+    void withdraw(double amount) {
+        if(getBalance() - amount < 100){
+            System.out.println("Operation not allowed, minimum balance is 100");
+        }
+        else{
+            super.withdraw(amount);
+        }
+    }
 }
