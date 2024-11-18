5fe78dcd78f2cf7b65b9c7d809d3d64c9d894332
brunombs <brunozs@msn.com>
2024-11-18 02:44:26 -0300

    adding exercise

---
+++ 05 - Object Oriented Programming/07 - Java Encapsulation/07 - Student - UNFINISHED/Main.java
@@ -0,0 +1,11 @@
+// Write a Java program to create a class called Student with private instance variables student_id, student_name, and grades. Provide public getter and setter methods to access and modify the student_id and student_name variables. However, provide a method called addGrade() that allows adding a grade to the grades variable while performing additional validation.
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student(1, "John Doe");
+        System.out.println("Student ID: " + student.getStudentID());
+        System.out.println("Student Name: " + student.getStudentName());
+        System.out.println("Student grade: " + student.g);
+        student.addGrade(5.4);
+    }
+}
---
+++ 05 - Object Oriented Programming/07 - Java Encapsulation/07 - Student - UNFINISHED/Student.java
@@ -0,0 +1,30 @@
+public class Student {
+    private int studentID;
+    private String studentName;
+    private double grade;
+
+    public Student(int studentID, String studentName) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void addGrade(double grade) {
+        this.grade += grade;
+    }
+}
