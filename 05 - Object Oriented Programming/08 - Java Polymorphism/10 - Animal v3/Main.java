3ca631ac22455fcc7e10258cfecd0299e11fd446
brunombs <brunozs@msn.com>
2024-11-21 02:08:18 -0300

    improving exercise

--- 05 - Object Oriented Programming/08 - Java Polymorphism/10 - Animal v3/Main.java
+++ 05 - Object Oriented Programming/08 - Java Polymorphism/10 - Animal v3/Main.java
@@ -1,3 +1,5 @@
+// Write a Java program to create a base class Animal with methods eat() and sound(). Create three subclasses: Lion, Tiger, and Panther. Override the eat() method in each subclass to describe what each animal eats. In addition, override the sound() method to make a specific sound for each animal.
+
 public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal();
