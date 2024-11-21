0b431ecaeb8b7871aaaaf20dca38f1e2f9d4a1c0
brunombs <brunozs@msn.com>
2024-11-21 02:08:28 -0300

    improving exercise

--- 05 - Object Oriented Programming/08 - Java Polymorphism/09 - BankAccount/Main.java
+++ 05 - Object Oriented Programming/08 - Java Polymorphism/09 - BankAccount/Main.java
@@ -1,3 +1,5 @@
+// Write a Java program to create a base class BankAccount with methods deposit() and withdraw(). Create two subclasses SavingsAccount and CheckingAccount. Override the withdraw() method in each subclass to impose different withdrawal limits and fees.
+
 public class Main {
     public static void main(String[] args) {
         BankAccount account = new BankAccount(500);
