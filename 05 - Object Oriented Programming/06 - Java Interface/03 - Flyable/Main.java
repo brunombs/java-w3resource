e5d67db57ff39d638cd817641167470048947713
brunombs <brunozs@msn.com>
2024-11-26 03:21:45 -0300

    improving exercise

--- 05 - Object Oriented Programming/06 - Java Interface/03 - Flyable/Main.java
+++ 05 - Object Oriented Programming/06 - Java Interface/03 - Flyable/Main.java
@@ -2,6 +2,11 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        Spacecraft spacecraft = new Spacecraft();
+        Airplane airplane = new Airplane();
+        Helicopter helicopter = new Helicopter();
+        spacecraft.fly();
+        airplane.fly();
+        helicopter.fly();
     }
 }
