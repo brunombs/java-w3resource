3b9aa2d91cdbd6fb6b1e883e73321090b821a462
brunombs <brunozs@msn.com>
2024-11-26 03:16:30 -0300

    adding exercise

---
+++ 05 - Object Oriented Programming/06 - Java Interface/02 - Animal/Main.java
@@ -0,0 +1,8 @@
+// Write a Java program to create an Animal interface with a method called bark() that takes no arguments and returns void. Create a Dog class that implements Animal and overrides speak() to print "Dog is barking".
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.bark();
+    }
+}
