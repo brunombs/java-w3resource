49493f40c6807cbe4348b4c5841ea367b7fde742
brunombs <brunozs@msn.com>
2024-12-02 16:13:18 -0300

    improving exercise

--- 03 - Java Control Flow/01 - Conditional Statement/13 - Cube/Cube.java
+++ 03 - Java Control Flow/01 - Conditional Statement/13 - Cube/Cube.java
@@ -1,13 +1,16 @@
+// Write a Java program to display the cube of the given number up to an integer.
+
 import java.util.Scanner;
 
 public class Cube {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.println("Input number of terms: ");
         int number = scanner.nextInt();
-        int cube = number * number * number;
 
-        System.out.println("The number is: " + number);
-        System.out.println("The cube is " + cube);
+        for (int i = 1; i <= number; i++) {
+            int cube = i * i * i;
+            System.out.println("The number is: " + i + " and the cube is: " + cube);
+        }
     }
 }
