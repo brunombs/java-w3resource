f8efeb2f3720aaf9056905c645da30bb7c8453d5
brunombs <brunozs@msn.com>
2024-12-02 16:01:57 -0300

    adding exercise

---
+++ 03 - Java Control Flow/01 - Conditional Statement/12 - 5 Numbers/FiveNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FiveNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int sum = 0;
+        while (count < 5) {
+            count++;
+            System.out.println("Enter a number: ");
+            int number = scanner.nextInt();
+            sum += number;
+        }
+        int average = sum / count;
+        System.out.println("The sum of 5 numbers is: " + sum);
+        System.out.println("The average is " + average);
+    }
+}
