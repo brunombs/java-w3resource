8e363c24cf86d744a46b56efb3119cb8bfa7c413
brunombs <brunozs@msn.com>
2025-01-13 23:53:59 -0300

    improving exercise

--- 05 - Object Oriented Programming/04 - Java Inheritance/11 - House/House.java
+++ 05 - Object Oriented Programming/04 - Java Inheritance/11 - House/House.java
@@ -2,4 +2,38 @@ public class House {
     private int numberOfRooms;
     private double area;
     private String address;
+
+    public House(int numberOfRooms, double area, String address) {
+        this.numberOfRooms = numberOfRooms;
+        this.area = area;
+        this.address = address;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double calculatePrice(double pricePerSquareMeter) {
+        return this.area * pricePerSquareMeter;
+    }
 }
