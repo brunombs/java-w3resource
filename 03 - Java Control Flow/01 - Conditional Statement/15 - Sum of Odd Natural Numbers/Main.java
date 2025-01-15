b202b0b7063f91d60cb6f2245eb975b30fa7ae3f
brunombs <brunozs@msn.com>
2025-01-14 22:58:54 -0300

    adding exercise :D

---
+++ 03 - Java Control Flow/01 - Conditional Statement/15 - Sum of Odd Natural Numbers/Main.java
@@ -0,0 +1,20 @@
+// Write a Java program that displays the sum of n odd natural numbers.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Input number of terms: ");
+        int number = scanner.nextInt();
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 1) {
+                sum += i;
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
