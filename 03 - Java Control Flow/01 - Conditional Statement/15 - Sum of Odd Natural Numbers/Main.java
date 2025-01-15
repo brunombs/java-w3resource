6ab341e91d397f812d08d7a4316ceec8564a0d43
brunombs <brunozs@msn.com>
2025-01-14 22:59:24 -0300

    improving exercise (:

--- 03 - Java Control Flow/01 - Conditional Statement/15 - Sum of Odd Natural Numbers/Main.java
+++ 03 - Java Control Flow/01 - Conditional Statement/15 - Sum of Odd Natural Numbers/Main.java
@@ -6,15 +6,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
+        int totalOfNumbers = 0;
         System.out.println("Input number of terms: ");
         int number = scanner.nextInt();
         for (int i = 1; i <= number; i++) {
             if (i % 2 == 1) {
                 sum += i;
+                totalOfNumbers++;
                 System.out.println(i);
             }
         }
 
         System.out.println("Sum: " + sum);
+        System.out.println("Total of numbers: " + totalOfNumbers);
     }
 }
