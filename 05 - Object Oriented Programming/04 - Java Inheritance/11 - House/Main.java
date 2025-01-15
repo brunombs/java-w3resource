4295af6fe3b7f3174fcab0286983e1b4e37b9e2b
brunombs <brunozs@msn.com>
2025-01-14 22:45:58 -0300

    error fix

--- 05 - Object Oriented Programming/04 - Java Inheritance/11 - House/Main.java
+++
@@ -1,9 +0,0 @@
-// Write a Java program to create a class called House with private instance variables address, numberOfRooms, and area. Provide public getter and setter methods to access and modify these variables. Add a method called calculatePrice() that returns the price of the house based on its area and a price per square meter.
-
-public class Main {
-    public static void main(String[] args) {
-        House house1 = new House(5, 140, "Street 2");
-
-        System.out.println("House with " + house1.getNumberOfRooms() + " rooms located on " + house1.getAddress().toLowerCase() + ", price: " + house1.calculatePrice(1300));
-    }
-}
