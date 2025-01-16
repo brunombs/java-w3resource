67f114e59fc1f67b6a059cda4da0742e15be96d2
brunombs <brunozs@msn.com>
2025-01-15 23:47:24 -0300

    improving exercise (:

--- 03 - Java Control Flow/01 - Conditional Statement/16 - Right-Angle Triangle Number Pattern/Main.java
+++ 03 - Java Control Flow/01 - Conditional Statement/16 - Right-Angle Triangle Number Pattern/Main.java
@@ -9,7 +9,10 @@ public class Main {
         int rows = scanner.nextInt();
 
         for (int i = 1; i <= rows; i++) {
-            System.out.print(i);
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
         }
     }
 }
