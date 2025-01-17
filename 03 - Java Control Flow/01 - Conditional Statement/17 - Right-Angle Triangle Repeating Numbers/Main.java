d1549880e38ef63f65e90adf645a117187047e3a
brunombs <brunozs@msn.com>
2025-01-16 21:45:43 -0300

    adding exercise (:

---
+++ 03 - Java Control Flow/01 - Conditional Statement/17 - Right-Angle Triangle Repeating Numbers/Main.java
@@ -0,0 +1,7 @@
+// Write a program in Java to make such a pattern like a right angle triangle with a number which repeats a number in a row.
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
