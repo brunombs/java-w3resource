37a24fb5885481bc86f9be026196ccfb18b9dc44
brunombs <brunozs@msn.com>
2025-01-19 23:48:01 -0300

    improving exercise (:

--- 03 - Java Control Flow/01 - Conditional Statement/17 - Right-Angle Triangle Repeating Numbers/Main.java
+++ 03 - Java Control Flow/01 - Conditional Statement/17 - Right-Angle Triangle Repeating Numbers/Main.java
@@ -7,9 +7,11 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
 
-        for (int i = 0; i < number; i++) {
-            for (int j = i + 1; j < number; j++) {
-                System.out.println(i);}
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
             }
+            System.out.println();
         }
     }
+}
